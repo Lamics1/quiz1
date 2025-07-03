7bd8f4263446ef184bf350366c34fb09c1bba6ce
Lamics1 <mr.lamics@gmail.com>
2025-07-03 10:56:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+//Write a Java program that accept three numbers from the user and print the largest number .
+
+//        int largest =0;
+//        System.out.println("Enter first numbers");
+//        int number1 =s.nextInt();
+//        System.out.println("Enter second numbers");
+//        int number2 =s.nextInt();
+//        System.out.println("Enter thread numbers");
+//        int number3 =s.nextInt();
+//
+//        if (number1 >number2 && number1 >number3){
+//            largest =number1;
+//        } else if (number2 > number1 && number2 >number3) {
+//
+//            largest =number2;
+//        }
+//        else {
+//            largest =number3;
+//        }
+//        System.out.println("the largest number is:"+largest);
+
+        //2 // Write a Java program that accept a String and a number from the user,then print the character in the given index .
+
+//        System.out.println("Enter string ");
+//        String word = s.nextLine();
+//        System.out.println("Enter number");
+//        int number =s.nextInt();
+//
+//        System.out.println(word.charAt(number));
+//
+
+        // Write a program to enter the numbers till the user wants and at the end it should display the sum entered .
+
+//        int total = 0;
+//
+//        System.out.println("Enter any number if done enter 0 for exit");
+//        while (true){
+//        int count = s.nextInt();
+//        total += count;
+//
+//        if (count == 0) {
+//            System.out.println("total number is " + total);
+//            break;
+//        }
+//    }
+//    }
+//}
+
+
+      //  Write a Java program to find positive and negative numbers of a given array:
+
+//        int[] number = {10, -21, 30, 31, -25};
+//
+//        for (int i = 0; i <= number.length - 1; i++) {
+//            if (number[i]> 0){
+//                System.out.println(number[i]+" is a positive number");
+//            }else {
+//                System.out.println(number[i]+ " is a negative number");
+//            }
+
+//Write a Java program to find a shortest word of a given array:
+
+//        String [] word ={"Tuwaiq","Bootcamp", "Student","JAVA"};
+//
+//        String shortest =word[0];
+//
+//        for (int i = 1 ; i<= word.length-1 ;i++){
+//
+//            if (word[i].length()<shortest.length()){
+//                shortest =word[i];
+//
+//            }else {
+//                word[i]=shortest;
+//            }
+//
+//        }
+//        System.out.println("The shortest word is :"+shortest);
+//        }
+//    }
+
+
